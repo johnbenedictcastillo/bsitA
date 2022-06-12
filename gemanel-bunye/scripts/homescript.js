a27c2aa8759c6d461e7f406c4aba82487a57794c
Bunye, Hannah Rae <hannahrae.bunye@lspu.edu.ph>
2022-06-12 15:14:49 +0000

    Create scripts folder

--- gemanel-bunye/scripts/homescript.js
+++
@@ -1,8 +0,0 @@
-function myFunction() {
-    var x = document.getElementById("myNav");
-    if (x.className === "navigation") {
-      x.className += " responsive";
-    } else {
-      x.className = "navigation";
-    }
-  }

4ded119a42e2bc510219b8d8f3d96caa5c49dc82
sharlyn-rancap-lspu <101610589+sharlyn-rancap-lspu@users.noreply.github.com>
2022-06-13 17:01:00 +0800

    Update main.js

--- acorda-rancap/scripts/main.js
+++ acorda-rancap/scripts/main.js
@@ -41,3 +41,26 @@
                             delay: (el, i) => 100 + 30 * i
                           });
                     
+var textWrapper = document.querySelector('.text1 ');
+                    textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+          var textWrapper = document.querySelector('.text1 ');
+               anime.timeline({loop: true})
+                          .add({
+                            targets: '.text1 .letter',
+                            translateX: [40,0],
+                            translateZ: 0,
+                            opacity: [0,1],
+                            easing: "easeOutExpo",
+                            duration: 1200,
+                            delay: (el, i) => 500 + 30 * i
+                          }).add({
+                            targets: '.text1 .letter',
+                            translateX: [0,-30],
+                            opacity: [1,0],
+                            easing: "easeInExpo",
+                            duration: 1100,
+                            delay: (el, i) => 100 + 30 * i
+                          });
+
+

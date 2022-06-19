43fe19ada75ffefadb8d5f4d4c0e650cbf29d6f3
sharlyn-rancap-lspu <101610589+sharlyn-rancap-lspu@users.noreply.github.com>
2022-06-19 16:24:26 +0800

    Create animate.js

---
+++ acorda-rancap/scripts/animate.js
@@ -0,0 +1,62 @@
+ar textWrapper = document.querySelector('.text');
+                    textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+                     anime.timeline({loop: true})
+                          .add({
+                            targets: '.text .letter',
+                            translateX: [40,0],
+                            translateZ: 0,
+                            opacity: [0,1],
+                            easing: "easeOutExpo",
+                            duration: 1200,
+                            delay: (el, i) => 500 + 30 * i
+                          }).add({
+                            targets: '.text .letter',
+                            translateX: [0,-30],
+                            opacity: [1,0],
+                            easing: "easeInExpo",
+                            duration: 1100,
+                            delay: (el, i) => 100 + 30 * i
+                          });
+var textWrapper = document.querySelector('.text1');
+                    textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+                     anime.timeline({loop: true})
+                          .add({
+                            targets: '.text1 .letter',
+                            translateX: [40,0],
+                            translateZ: 0,
+                            opacity: [0,1],
+                            easing: "easeOutExpo",
+                            duration: 1200,
+                            delay: (el, i) => 500 + 30 * i
+                          }).add({
+                            targets: '.text1 .letter',
+                            translateX: [0,-30],
+                            opacity: [1,0],
+                            easing: "easeInExpo",
+                            duration: 1100,
+                            delay: (el, i) => 100 + 30 * i
+                          });
+                        
+  var textWrapper = document.querySelector('.qoute');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+                        anime.timeline({loop: true})
+                        .add({
+                            targets: '.qoute .letter',
+                            translateX: [40,0],
+                            translateZ: 0,
+                            opacity: [0,1],
+                            easing: "easeOutExpo",
+                            duration: 1200,
+                            delay: (el, i) => 500 + 30 * i
+                        }).add({
+                            targets: '.qoute .letter',
+                            translateX: [0,-30],
+                            opacity: [1,0],
+                            easing: "easeInExpo",
+                            duration: 1100,
+                            delay: (el, i) => 100 + 30 * i
+                        });                        
+                                                                

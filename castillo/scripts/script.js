3490774beedc21d78b2a2073535a1b6051d1626c
johnc <john.castillo@jajja.com>
2022-06-20 13:27:19 +0800

    Updated script

--- castillo/scripts/script.js
+++ castillo/scripts/script.js
@@ -3,6 +3,20 @@
 const connection = supabase.createClient('https://zzivlqstynxhbfabxhpi.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inp6aXZscXN0eW54aGJmYWJ4aHBpIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NTQ0MDE5NzAsImV4cCI6MTk2OTk3Nzk3MH0.wntGDWfy7wcuhepHw2XwbInty25vUG_6AI4U4GsJTKg')
 
 
+async function saveImage(pic){
+  const { data, error } = await connection
+  .storage
+  .from('avatars')
+  .upload('public/profie.png', pic, {
+    cacheControl: '3600',
+    upsert: false
+  })
+  if(data) 
+    console.log(data)
+  if(error) 
+    console.log(error)
+
+}
 
 $(document).ready(function(){
 
@@ -14,16 +28,6 @@ $(document).ready(function(){
       let lname = $('#last-name').val();
       let email = $('#email').val();
       let pic = $('#fileinput').prop('files');
-      const { data, error } = await connection
-      .storage
-      .from('avatars')
-      .upload('public/profie.png', pic, {
-        cacheControl: '3600',
-        upsert: false
-      })
-      if(data) 
-        console.log(data)
-      if(error) 
-        console.log(error)
+      saveImage(pic)
     });
   });

aeba9e3b2c7d20386cb66080ec048df978db16ff
Joseph0203 <101610343+Joseph0203@users.noreply.github.com>
2022-06-21 11:12:51 +0800

    Update script.js

--- camposano-camposano/script/script.js
+++ camposano-camposano/script/script.js
@@ -1,20 +1,3 @@
-anime.timeline({loop: true})
-  .add({
-    targets: '.ml15 .word',
-    scale: [14,1],
-    opacity: [0,1],
-    easing: "easeOutCirc",
-    duration: 500,
-    delay: (el, i) => 500 * i
-  }).add({
-    targets: '.ml15',
-    opacity: 0,
-    duration: 600,
-    easing: "easeOutExpo",
-    delay: 1000
-  });
-
-
 /* hide nav */
 
 var prevScrollpos = window.pageYOffset;

ce97e6b7e1984235978e3604241c66ca99e2741c
raina-gemanel-lspu <101684251+raina-gemanel-lspu@users.noreply.github.com>
2022-07-06 03:38:09 +0800

    Create index.php

---
+++ gemanel-bunye/php/pages/index.php
@@ -0,0 +1,67 @@
+<?php
+
+include_once 'header.php';
+
+?>
+
+
+			<div class="row productpart">
+				<h3 class="h3font">LATEST MENU</h3>
+				<div class="col-md-5 col-sm-12">
+					<figure class="figure"  id="latestpic1">
+						<img src="palitaw.jpg" alt="Palitaw" class="product">
+						<p class="productfont">Palitaw</p>
+					</figure>
+				</div>
+				<div class="col-md-2 col-sm-0"></div>
+				<div class="col-md-5 col-sm-12">
+					<figure class="figure"  id="latestpic2">
+						<img src="butchi.jpg" alt="Butchi" class="product">
+						<p class="productfont">Butchi</p>
+					</figure>
+				</div>
+			</div>
+			<div class="row schedulepart">
+				<div class="col-md-12 col-sm-12">
+					<h3 class="h3font">SCHEDULES</h3>
+					<table class="table table-striped table-bordered">
+						<thead>
+						  <tr class="table-success border-dark">
+							<th scope="col" class="thfont">Product</th>
+							<th scope="col" class="thfont">Day's of Available</th>
+						  </tr>
+						</thead>
+						<tbody>
+						  <tr>
+							<td scope="row" class="tdfont">Tuna Aglio Olio</th>
+							<td class="tdfont">Wednesday</td>
+						  </tr>
+						  <tr>
+							<td scope="row" class="tdfont">Spaghetti</th>
+							<td class="tdfont">Monday</td>
+						  </tr>
+						  <tr>
+							<td scope="row" class="tdfont">Butchi</th>
+							<td class="tdfont">Monday and Wednesday</td>
+						  </tr>
+						  <tr>
+							<td scope="row" class="tdfont">Palitaw</th>
+							<td class="tdfont">Monday and Wednesday</td>
+						  </tr>
+						  <tr>
+							<td scope="row" class="tdfont">Chicken Popper</th>
+							<td class="tdfont">Saturday</td>
+						  </tr>
+						  <tr>
+							<td scope="row" class="tdfont">Pork Tonkatsu</th>
+							<td class="tdfont">Sunday</td>
+						  </tr>
+						</tbody>
+					  </table>
+				</div>
+			</div>
+<?php
+
+include_once 'footer.php';
+
+?>

ed1a4ce8857141593eb08144f34798acce737107
sharlyn-rancap-lspu <101610589+sharlyn-rancap-lspu@users.noreply.github.com>
2022-07-07 16:52:19 +0800

    Update animate.js

--- acorda-rancap/scripts/animate.js
+++ acorda-rancap/scripts/animate.js
@@ -58,5 +58,81 @@ textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='let
                             easing: "easeInExpo",
                             duration: 1100,
                             delay: (el, i) => 100 + 30 * i
-                        });                        
-                                                                
+                        });                     
+
+
+                        var popupViews = document.querySelectorAll('.popup-view');
+                        var popupBtns = document.querySelectorAll('.popup-btn');
+                        var closeBtns = document.querySelectorAll('.close-btn');
+                    
+                      
+                        var popup = function(popupClick){
+                          popupViews[popupClick].classList.add('active');
+                        }
+                    
+                        popupBtns.forEach((popupBtn, i) => {
+                          popupBtn.addEventListener("click", () => {
+                            popup(i);
+                          });
+                        });
+                    
+                        
+                        closeBtns.forEach((closeBtn) => {
+                          closeBtn.addEventListener("click", () => {
+                            popupViews.forEach((popupView) => {
+                              popupView.classList.remove('active');
+                            });
+                          });
+                        });
+                  
+                  
+                    
+                    const select = (el, all = false) => {
+                      el = el.trim()
+                      if (all) {
+                        return [...document.querySelectorAll(el)]
+                      } else {
+                        return document.querySelector(el)
+                      }
+                    }
+                  
+                    const on = (type, el, listener, all = false) => {
+                      let selectEl = select(el, all)
+                      if (selectEl) {
+                        if (all) {
+                          selectEl.forEach(e => e.addEventListener(type, listener))
+                        } else {
+                          selectEl.addEventListener(type, listener)
+                        }
+                      }
+                    }
+                  
+                        window.addEventListener('load', () => {
+                      let menuContainer = select('.menu-container');
+                      if (menuContainer) {
+                        let menuIsotope = new Isotope(menuContainer, {
+                          itemSelector: '.menu-item',
+                          layoutMode: 'fitRows'
+                        });
+                  
+                        let menuFilters = select('#menu-flters li', true);
+                  
+                        on('click', '#menu-flters li', function(e) {
+                          e.preventDefault();
+                          menuFilters.forEach(function(el) {
+                            el.classList.remove('filter-active');
+                          });
+                          this.classList.add('filter-active');
+                  
+                          menuIsotope.arrange({
+                            filter: this.getAttribute('data-filter')
+                          });
+                          menuIsotope.on('arrangeComplete', function() {
+                            AOS.refresh()
+                          });
+                        }, true);
+                      }
+                  
+                    });
+                  
+                      

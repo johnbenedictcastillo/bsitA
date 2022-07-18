e39392d8a62f80ae28b7028ca46e28420cd9216e
elirose-dalit-lspu <101610497+elirose-dalit-lspu@users.noreply.github.com>
2022-07-18 22:02:26 +0800

    Update login.js

--- merin-dalit-zantua/login.js
+++ merin-dalit-zantua/login.js
@@ -1,1 +1,37 @@
 
+
+
+const connection = supabase.createClient(PROJECT_URL,API_KEY);
+
+
+var button = document.getElementById("loginform");
+button.addEventListener("click", saveUser);
+
+async function saveUser(){
+
+    var username = document.getElementById("username");
+    var password = document.getElementById("password");
+    console.log(username.value)
+    console.log(password.value)
+  const { data, error } = await connection.from("user").select('*').eq('username', username.value).eq('password', password.value);
+    if (data){
+        if (data.length > 0){
+            console.log(data)
+            window.alert("Login Successfully!");
+            sessionStorage.setItem("username", data[0].username);
+            sessionStorage.setItem("password", data[0].password);
+            location.replace('/bolinas-leynes/pages/productpage.html')
+        }else{
+            window.alert("Failed to Login.");
+            username.value = null;
+            password.value = null;
+
+        }
+    }
+    if(error){
+        console.log(error)
+        window.alert("Failed to Login.");
+        username.value = null;
+        password.value = null;
+    }
+}
